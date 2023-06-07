562ef0e22a31d871b8faa99e2c70ac41f1d27503
Sharanya <103178789+Sharanya9357@users.noreply.github.com>
2023-06-07 19:21:05 +0530

    Update CONTROL UNIT.v

--- CONTROL UNIT.v
+++ CONTROL UNIT.v
@@ -1,37 +1,38 @@
-module CONTROL(
-    input [6:0] funct7,        // 7-bit input representing the funct7 field
-    input [2:0] funct3,        // 3-bit input representing the funct3 field
-    input [6:0] opcode,        // 7-bit input representing the opcode field
-	output reg [3:0] alu_control_signal,       // 4-bit output controlling the ALU operation
-    output reg regwrite_control        // Output controlling the register write operation
+module control_unit(
+    input [11:0] imm,
+    input [2:0] funct3,
+    input [6:0] opcode,
+    output reg [3:0] alu_control_signal, // for alu 
+    output reg regwrite_control_signal
 );
-    always @(funct3 or funct7 or opcode)
+    always @(funct3 or imm or opcode)
     begin
-        if (opcode == 7'b0110011) begin // R-type instructions
+        if (opcode == 7'b0110011) begin // I-type instructions
 
-            regwrite_control = 1;       // Enable register write
-
+            regwrite_control_signal = 1;
             case (funct3)
                 0: begin
-                    if (funct7 == 0)
-                        alu_control_signal = 4'b0010; // ALU control for ADD operation
-                    else if (funct7 == 32)
-                        alu_control_signal = 4'b0100; // ALU control for SUB operation
+                    if(imm == 0)
+                    alu_control_signal = 4'b0000; // ADDI
+                    else if(imm == 32)
+                    alu_control_signal = 4'b0010; // SUBI
                 end
-                6: alu_control_signal = 4'b0001; // ALU control for OR operation
-                7: alu_control_signal = 4'b0000; // ALU control for AND operation
-                1: alu_control_signal = 4'b0011; // ALU control for SLL operation (Shift Left Logical)
-                5: alu_control_signal = 4'b0101; // ALU control for SRL operation (Shift Right Logical)
-		2: alu_control_signal = 4'b0110; // ALU control for MUL operation
-		4: alu_control_signal = 4'b0111; // ALU control for XOR operation
+                1: alu_control_signal = 4'b0001; // SLLI
+//             2: alu_control_signal = 4'b0111; // MULI
+//                3: begin
+//					if(imm == 8)
+//                    alu_control_signal = 4'b1010; //lr.d
+//                    else if (funct7 == 12)
+//                    alu_control_signal = 4'b1100; //sc.d
+//				end
+				4: alu_control_signal = 4'b0100; // XORI
+				5: alu_control_signal = 4'b0101; // SRLI
+                6: alu_control_signal = 4'b0110; // ORI
+                7: alu_control_signal = 4'b0111; // ANDI
+				
             endcase
 
-        end
+      end
+
     end
-endmodule
-
-
-
-
-
-
+    endmodule
